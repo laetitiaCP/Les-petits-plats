0939045487c5d358875e592be686215de1676016
laetitiaCP <laetitia.cendra.pro@gmail.com>
2023-06-06 18:34:02 +0200

    <feat><Mise en place algos de recherche>

--- App/js/Composants/tags.js
+++ App/js/Composants/tags.js
@@ -1,18 +1,100 @@
 const _filterTags = document.getElementById("filter__tags");
+const inputIngredients = document.getElementById("input-ingredients");
+const inputAppliances = document.getElementById("input-appliances");
+const inputUstensils = document.getElementById("input-ustensils");
+
+/**
+ * Appelée par locElementList dans la liste des ingrédients, appareils et ustensiles
+ * @param parElement
+ */
+
 function addFilterTags(parElement){
+    let locNameElement = parElement.textContent;
     let locSplitClassNameElement = String(parElement.className).split("-")
     let locTag = document.createElement("span");
     let locClassName = "tags-" + locSplitClassNameElement[2];
     locTag.setAttribute("class", locClassName);
-    locTag.textContent = parElement.textContent;
+    locTag.textContent = locNameElement;
 
     let locCrossIcon = document.createElement("i");
     locCrossIcon.setAttribute("class", "bi bi-x-circle");
     locCrossIcon.onclick = () => {
         locTag.remove();
+        deleteTag();
     }
 
     locTag.appendChild(locCrossIcon);
 
-    _filterTags.appendChild(locTag)
+    _filterTags.appendChild(locTag);
+    let valueInputPrincipal = document.getElementById("search-input").value;
+console.log(valueInputPrincipal)
+    if(valueInputPrincipal === "") {
+        removeAllSectionsResearch(recipeSection, ingredientElement, applianceElement, ustensilElement);
+        research(locNameElement.toLowerCase());
+        updateAllInputs(ingredientsSet, ingredientElement, appliancesSet, applianceElement, ustensilsSet, ustensilElement);
+    } else {
+        removeAllSectionsResearch(recipeSection, ingredientElement, applianceElement, ustensilElement);
+        researchToClickTags(parElement.textContent.toLowerCase());
+        updateAllInputs(ingredientsSet, ingredientElement, appliancesSet, applianceElement, ustensilsSet, ustensilElement);
+    }
 }
+
+function researchInTags(parInput, parSet, parElementHtml) {
+    const updatedSet = new Set();
+    parInput.addEventListener("input", e => {
+        updatedSet.clear();
+       let locIngredientEntered = e.target.value.toLowerCase();
+       for(let item of parSet) {
+           if(String(item).toLowerCase().includes(locIngredientEntered)) {
+               updatedSet.add(item);
+           }
+       }
+       console.log(updatedSet)
+       removeAllChildNodes(parElementHtml);
+       displayListFilters(updatedSet, parElementHtml);
+    });
+}
+
+function researchToClickTags(parString) {
+    let tagsSet = new Set();
+    for(let element of recipesSorted) {
+        let isContained = false;
+        if ((element.name).toLowerCase().includes(parString)) {
+            tagsSet.add(element);
+            continue;
+        } else {
+            for (let i = 0; i < element.ingredients.length; i++) {
+                if ((element.ingredients[i].ingredient).toLowerCase().includes(parString)) {
+                    isContained = true;
+                    tagsSet.add(element);
+                    break;
+                }
+            }
+        }
+        if (false == isContained) {
+            if ((element.description).toLowerCase().includes(parString)) {
+                isContained = true;
+                tagsSet.add(element)
+            }
+        }
+    }
+    console.log(tagsSet)
+    return displayRecipesDetails(tagsSet);
+}
+
+function deleteTag() {
+    let valueInputPrincipal = document.getElementById("search-input").value;
+    let listTags = document.getElementById("filter__tags").children;
+    if(listTags.length === 0) {
+        removeAllSectionsResearch(recipeSection, ingredientElement, applianceElement, ustensilElement);
+        research(valueInputPrincipal);
+        updateAllInputs(ingredientsSet, ingredientElement, appliancesSet, applianceElement, ustensilsSet, ustensilElement);
+    } else {
+        removeAllSectionsResearch(recipeSection, ingredientElement, applianceElement, ustensilElement);
+        for(let i=0; i<listTags.length; i++) {
+            researchToClickTags(listTags[i].outerText.toLowerCase());
+        }
+        updateAllInputs(ingredientsSet, ingredientElement, appliancesSet, applianceElement, ustensilsSet, ustensilElement);
+    }
+}
+
--- App/js/Research/research.js
+++ App/js/Research/research.js
@@ -1,7 +1,71 @@
 
-
+const recipesSorted = new Set();
 function research(parEntry) {
-    const recipes = [];
+    recipesSorted.clear();
+    let locWordsToSearch = parEntry.split(" ");
 
-    return recipes;
+    const recipes = getData();
+    let isContained = false;
+    let areAllWordsContained = false;
+
+    for(let i=0; i<recipes.length; i++) {
+        isContained = false;
+        areAllWordsContained = true;
+
+        for (let j=0; j<locWordsToSearch.length; j++) {
+            isContained = false;
+            if (locWordsToSearch[j] === "") {
+                continue;
+            }
+
+            if ((recipes[i].name).toLowerCase().includes(locWordsToSearch[j])) {
+                isContained = true;
+                areAllWordsContained &= true;
+                continue;
+            } else {
+                for (let k = 0; k < recipes[i].ingredients.length; k++) {
+                    if ((recipes[i].ingredients[k].ingredient).toLowerCase().includes(locWordsToSearch[j])) {
+                        isContained = true;
+                        areAllWordsContained &= true;
+                        break;
+                    }
+                }
+                if (false == isContained) {
+                    if ((recipes[i].description).toLowerCase().includes(locWordsToSearch[j])) {
+                        isContained = true;
+                        areAllWordsContained &= true;
+                    } else {
+                        isContained = false;
+                        areAllWordsContained = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if(areAllWordsContained) {
+            recipesSorted.add(recipes[i]);
+        }
+    }
+    console.log(recipesSorted.size)
+    if(recipesSorted.size === 0) {
+        return noResult();
+    } else {
+        return displayRecipesDetails(recipesSorted);
+    }
+}
+
+function removeAllSectionsResearch(parPrincipalResearch, parIngredientsResearch, parApplianceResearch, parUstensilsResearch) {
+    removeAllChildNodes(parPrincipalResearch);
+    removeAllChildNodes(parIngredientsResearch);
+    removeAllChildNodes(parApplianceResearch);
+    removeAllChildNodes(parUstensilsResearch);
+    ingredientsSet.clear();
+    appliancesSet.clear();
+    ustensilsSet.clear();
+}
+
+function updateAllInputs(parFirstSet, parFirstElement, parSecondSet, parSecondElement, parThirdSet, parThirdElement) {
+    displayListFilters(parFirstSet, parFirstElement);
+    displayListFilters(parSecondSet, parSecondElement);
+    displayListFilters(parThirdSet, parThirdElement);
 }
