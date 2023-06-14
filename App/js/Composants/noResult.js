3ec02cfe892374a955b3c4ee06f349f67ca16ebb
laetitiaCP <laetitia.cendra.pro@gmail.com>
2023-06-14 11:21:51 +0200

    Revert "<feat><Mise en place algos de recherche>"
    This reverts commit 0939045487c5d358875e592be686215de1676016.

--- App/js/Composants/noResult.js
+++
@@ -1,6 +0,0 @@
-function noResult() {
-    const spanNoResult = document.createElement("span");
-    spanNoResult.setAttribute("class", "span-noResult");
-    spanNoResult.textContent = "Aucune recette ne correspond à votre critère... vous pouvez chercher \"tarte aux pommes\", \"poisson\", etc.";
-    recipeSection.appendChild(spanNoResult);
-}
